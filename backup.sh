b7a6b732fafa106f1d5d8868439c7581a4a3d299
Tomek Paczkowski <tomek@hauru.eu>
2022-09-05 16:24:30 +0100

    Initial archive for the DUTH 2014 website

---
+++ backup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+rm -rf _build
+mkdir _build
+
+cd _build
+
+# Mirror the whole 2014 website, including some images stored in AWS
+wget \
+  --convert-links \
+  --html-extension \
+  --no-check-certificate \
+  --no-parent \
+  --page-requisites \
+  --recursive \
+  --user-agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36' \
+  -w 0.2 \
+  --restrict-file-names=windows \
+  -H --domains 2014.djangounderthehood.com,duth-uploads.s3.amazonaws.com \
+  http://2014.djangounderthehood.com
+
+# Images from AWS include signatures, let's strip them
+rename 's/@.*//' duth-uploads.s3.amazonaws.com/*
+
+# Move images from AWS to statics folder
+mv duth-uploads.s3.amazonaws.com 2014.djangounderthehood.com/static/
+
+# Replace all mentions to the AWS folder and strip signatures, all in one lovely line
+grep -rl duth-uploads.s3.amazonaws.com . | xargs sed -e 's/\.\.\/duth-uploads.s3.amazonaws.com/static\/duth-uploads.s3.amazonaws.com/' -e 's/@[^"]*//' -i ''
+
+cd ..
+
+mkdir -p docs
+cp -r _build/2014.djangounderthehood.com/* docs/
